7b785c12fbcb07d9f387818bac753ac34aae789f
S Solabannavar Smitha <SSolabannavar.Smitha@in.bosch.com>
2022-03-03 18:55:11 +0530

    calculateStats() implemented to calculate max, min and avg for given array of numbers

--- statistics.py
+++ statistics.py
@@ -1,3 +1,23 @@
 
 def calculateStats(numbers):
-  return None
+    result = {}
+    result["max"] = 0
+    result["avg"] = 0
+    result["min"] = 0
+    if(len(numbers) > 0):
+        sum = 0
+        for i in range(len(numbers)):
+            if(i == 0):
+                result["max"] = numbers[i]
+                result["min"] = numbers[i]
+            if(result["max"] < numbers[i]):
+                result["max"] = numbers[i]
+            if(result["min"] > numbers[i]):
+                print(numbers[i]+"------min")
+                result["min"] = numbers[i]
+            sum += numbers[i]
+        result["avg"] = sum/len(numbers)
+        return result
+    else:
+        result["avg"] = float("Nan")
+    return result
